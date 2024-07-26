1a2880c24824ccf762db9597194ebc653d933733
brownstracey <p.browns@alustudent.com>
2024-07-26 15:11:53 +0300

    mai.py

---
+++ Grade book app.py
@@ -0,0 +1,116 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/LENOVO/OneDrive/Desktop/student.py =============
+>>> 
+============== RESTART: C:/Users/LENOVO/OneDrive/Desktop/course.py =============
+>>> 
+============ RESTART: C:/Users/LENOVO/OneDrive/Desktop/gradebook.py ============
+>>> 
+=============== RESTART: C:/Users/LENOVO/OneDrive/Desktop/main.py ==============
+
+Grade Book Application
+1. Add Student
+2. Add Course
+3. Register Student for Course
+4. Calculate GPA
+5. Calculate Ranking
+6. Search by Grade
+7. Generate Transcript
+8. Exit
+Enter your choice: 1
+Enter student email: p.browns@alustudent.com
+Enter student names: Prudence Browns
+Student added successfully.
+
+Grade Book Application
+1. Add Student
+2. Add Course
+3. Register Student for Course
+4. Calculate GPA
+5. Calculate Ranking
+6. Search by Grade
+7. Generate Transcript
+8. Exit
+Enter your choice: 2
+Enter course name: Software Engineering
+Enter trimester: 2
+Enter credits: 3
+Enter grade point (e.g., 4.0 for A, 3.0 for B, etc.): 4.0
+Course added successfully.
+
+Grade Book Application
+1. Add Student
+2. Add Course
+3. Register Student for Course
+4. Calculate GPA
+5. Calculate Ranking
+6. Search by Grade
+7. Generate Transcript
+8. Exit
+Enter your choice: 3
+Enter student email: p.browns@alustudent.com
+Enter course name: Software Engineering
+Student registered for course successfully.
+
+Grade Book Application
+1. Add Student
+2. Add Course
+3. Register Student for Course
+4. Calculate GPA
+5. Calculate Ranking
+6. Search by Grade
+7. Generate Transcript
+8. Exit
+Enter your choice: 4
+GPA calculated successfully.
+
+Grade Book Application
+1. Add Student
+2. Add Course
+3. Register Student for Course
+4. Calculate GPA
+5. Calculate Ranking
+6. Search by Grade
+7. Generate Transcript
+8. Exit
+Enter your choice: 5
+1. Prudence Browns - GPA: 4.0
+
+Grade Book Application
+1. Add Student
+2. Add Course
+3. Register Student for Course
+4. Calculate GPA
+5. Calculate Ranking
+6. Search by Grade
+7. Generate Transcript
+8. Exit
+Enter your choice: 6
+Enter grade point to search (e.g., 4.0 for A): 4.0
+Prudence Browns - Email: p.browns@alustudent.com
+
+Grade Book Application
+1. Add Student
+2. Add Course
+3. Register Student for Course
+4. Calculate GPA
+5. Calculate Ranking
+6. Search by Grade
+7. Generate Transcript
+8. Exit
+Enter your choice: 7
+Enter student email: p.browns@alustudent.com
+Transcript for Prudence Browns
+Software Engineering: Grade Point 4.0
+GPA: 4.0
+
+Grade Book Application
+1. Add Student
+2. Add Course
+3. Register Student for Course
+4. Calculate GPA
+5. Calculate Ranking
+6. Search by Grade
+7. Generate Transcript
+8. Exit
